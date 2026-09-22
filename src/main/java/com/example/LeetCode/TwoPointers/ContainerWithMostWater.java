e9bbc57817e73d0b6a2745886b3418acf74d19ef
AlisaBackend <aspelpal7@gmail.com>
2026-09-22 19:33:30 +0300

    LeetCodeTwoPointer

--- src/main/java/com/example/LeetCode/TwoPointers/ContainerWithMostWater.java
+++ src/main/java/com/example/LeetCode/TwoPointers/ContainerWithMostWater.java
@@ -2,4 +2,25 @@ package com.example.LeetCode.TwoPointers;
 
 public class ContainerWithMostWater {
     
+    public int maxArea(int[] height) {
+          int left = 0;
+        int right = height.length - 1;
+        int max = 0;
+
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            int w = right - left;
+            int area = h * w;
+
+            if (area > max) max = area;
+
+            //двигаем указатель с меньшей высотой
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
 }
--- src/main/java/com/example/LeetCode/TwoPointers/ThreeSum.java
+++ src/main/java/com/example/LeetCode/TwoPointers/ThreeSum.java
@@ -1,5 +1,46 @@
 package com.example.LeetCode.TwoPointers;
 
+import java.util.*;
+
 public class ThreeSum {
     
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            // если самый маленький элемент > 0, дальше сумму 0 не получить
+            if (nums[i] > 0) break;
+
+            // пропускаем дубликаты для первого элемента
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = n - 1;
+            int target = -nums[i];
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+
+                if (sum == target) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // пропускаем дубликаты слева и справа
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
 }
